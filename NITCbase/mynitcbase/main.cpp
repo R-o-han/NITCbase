5f4e9d493f2054fbcb7d4d66cb62ffb610ee4d49
rohan <rohankrajendran@gmail.com>
2025-01-06 16:09:27 +0530

    Stage 2 Complete

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -3,26 +3,85 @@
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 #include <bits/stdc++.h>
-#include <cstring>
 using namespace std;
+
+
+int printAttributeCatalog(){
+    // create objects for the relation catalog and attribute catalog
+    RecBuffer relCatBuffer(RELCAT_BLOCK);
+    RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+    // load the headers of both the blocks into relCatHeader and attrCatHeader.
+    HeadInfo relCatHeader;
+    HeadInfo attrCatHeader;
+
+  // (we will implement these functions later)
+    relCatBuffer.getHeader(&relCatHeader);
+    attrCatBuffer.getHeader(&attrCatHeader);
+
+    for (int i=0, attrCatSlotIndex = 0;i<relCatHeader.numEntries;i++) {
+
+      Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+      relCatBuffer.getRecord(relCatRecord, i);
+
+      printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+      for (int j=0;j<relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal;j++,attrCatSlotIndex++) {
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord,attrCatSlotIndex);
+        // declare attrCatRecord and load the attribute catalog entry into it
+
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)==0) {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
+        if(attrCatSlotIndex == attrCatHeader.numSlots-1){
+          attrCatSlotIndex=-1;
+          attrCatBuffer=RecBuffer (attrCatHeader.rblock);
+          attrCatBuffer.getHeader(&attrCatHeader);
+        }
+
+      }
+      printf("\n");
+    }
+
+    return 0;
+}
+
+void updateAttributeName(const char* relName, const char* oldAttrName, const char* newAttrName){
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+  HeadInfo attrCatHeader;
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for(int i=0;i<attrCatHeader.numEntries;i++){
+    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+    attrCatBuffer.getRecord(attrCatRecord,i);
+    
+    if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relName)==0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,oldAttrName)==0){
+      strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,newAttrName);
+      attrCatBuffer.setRecord(attrCatRecord,i);
+      cout<<"Updated Successfully\n";
+      break;
+    }
+    // reaching at the end of the block, and thus loading
+		// the next block and setting the attrCatHeader & recIndex
+    if(i==attrCatHeader.numSlots-1){
+      i=-1;
+      attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+      attrCatBuffer.getHeader(&attrCatHeader);
+    }
+  }
+  
+}
+
 
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   
   Disk disk_run;
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  //Now, buffer[20] = 'h', buffer[21] = 'e' ...
-  Disk::writeBlock(buffer, 7000);
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  cout << message2;
-  return 0;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-  return FrontendInterface::handleFrontend(argc, argv);
+ printAttributeCatalog();
+  // updateAttributeName("Students","Class","Batch");
+  // printAttributeCatalog();
+  
 }
